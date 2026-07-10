c2ba0d0d21cb9cfa377edde72a29cb99e11d320c
cschibelsky-star <cschibelsky@gmail.com>
2026-07-10 08:32:10 -0300

    api: adicionar log de distribuição de vídeo

---
+++ public/api/social/video-distribution-log.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+use TVSumare\Api\Auth;
+use TVSumare\Api\JsonResponse;
+use TVSumare\Automation\AutomationLog;
+use TVSumare\Storage\JsonStore;
+
+Auth::requireToken();
+$input = json_decode(file_get_contents('php://input') ?: '[]', true);
+$payload = is_array($input) ? $input : [];
+
+$log = new AutomationLog(new JsonStore(__DIR__ . '/../../../data'));
+$log->add('video_distribution', $payload, 'registered');
+
+JsonResponse::send(['ok' => true, 'message' => 'Distribuição de vídeo registrada.']);
